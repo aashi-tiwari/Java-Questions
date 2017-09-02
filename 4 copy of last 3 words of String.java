bbc8831671fa80d2fbb22c02a5e4bfe2cc4218ae
Aashi Tiwari <aashi.tiwari@sjsu.edu>
2017-09-01 18:30:25 -0700

    Create 4 copy of last 3 words of String.java

---
+++ 4 copy of last 3 words of String.java
@@ -0,0 +1,13 @@
+package stack;
+
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String args[]) {
+      String s ="3.0";
+      System.out.println(s.substring(s.length()-3,s.length())+s.substring(s.length()-3,s.length())+s.substring(s.length()-3,s.length()));
+      
+	}
+
+}
+
